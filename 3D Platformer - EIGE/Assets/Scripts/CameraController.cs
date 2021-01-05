132995379f87bf20e66030b9f0832e60c5c01e4c
lNoro <okay.uenal@tum.de>
2021-01-05 17:52:01 +0100

    Added TinyFire, Build Bonfire, Made Camera "collide" with Walls
    -Imported Asset Pack for ParticleSystem
    -Build Prefab Bonfire_Lit
    -Adjusted Camera AwayDistance according if it hits Walls

--- 3D Platformer - EIGE/Assets/Scripts/CameraController.cs
+++ 3D Platformer - EIGE/Assets/Scripts/CameraController.cs
@@ -9,13 +9,41 @@ public class CameraController : MonoBehaviour
     public float DistanceUp;
     public float Smooth;
     public Transform FollowObject;
+    public LayerMask CameraCollide;
 
     private Vector3 m_ToPosition;
+    private float m_DistanceAway;
 
     private void LateUpdate()
     {
-        m_ToPosition = FollowObject.position + Vector3.up * DistanceUp - FollowObject.forward * DistanceAway;
+        CheckDistanceAway();
+        m_ToPosition = FollowObject.position + Vector3.up * DistanceUp - FollowObject.forward * m_DistanceAway;
         transform.position = Vector3.Lerp(transform.position, m_ToPosition, Time.deltaTime * Smooth);
         transform.LookAt(FollowObject);
+    }
+
+    private void CheckDistanceAway()
+    {
+        bool hit = Hit(DistanceAway);
+
+        if (hit)
+        {
+            float step = 0f;
+            while (hit)
+            {
+                step += .005f;
+                m_DistanceAway = Mathf.Lerp(DistanceAway, DistanceAway - step, 1);
+                hit = Hit(m_DistanceAway);
+            }
+        }
+        else
+        {
+            m_DistanceAway = DistanceAway;
+        }
+    }
+
+    private bool Hit(float distance_p)
+    {
+        return Physics.Raycast(FollowObject.position, FollowObject.forward * -1f, distance_p, CameraCollide);
     }
 }

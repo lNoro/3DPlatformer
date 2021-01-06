dde8ecba9d4112083aed02fa51b7461f3826dbcd
lNoro <okay.uenal@tum.de>
2021-01-06 14:43:03 +0100

    New Lightmaps, adjusted Gate, adjusted Landing
    -New Lightwalls in Gate
    -Player slows on Landing
    -Made some Prefabs

---
+++ 3D Platformer - EIGE/Assets/Animations/AnimationBehaviour/Char/Landing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : StateMachineBehaviour
+{
+    private float m_RunVelocityOrig;
+
+    private float m_RunVelocityLanding;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_RunVelocityOrig = animator.GetComponentInParent<PlayerController>().MoveSetting.RunVelocity;
+        m_RunVelocityLanding = m_RunVelocityOrig / 1.5f;
+        animator.GetComponentInParent<PlayerController>().MoveSetting.RunVelocity = m_RunVelocityLanding;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+        animator.GetComponentInParent<PlayerController>().MoveSetting.RunVelocity = m_RunVelocityOrig;
+    }
+}
